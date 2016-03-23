bdf0b09d7436fde8e3550ef6617951d3dbe703e8
pengyuntao <pengyuntao@le.com>
2016-03-23 17:50:34 +0800

    添加TV焦点框

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TvFocusDemo
 一个简单的Android系统电视的通用焦点框实现
+
+![Alt text](preview.gif)
---
+++ app/src/main/java/com/yuntao/tvfocus/app/MainActivity.java
@@ -0,0 +1,44 @@
+package com.yuntao.tvfocus.app;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+
+public class MainActivity extends Activity {
+
+    FocusLayout mFocusLayout;//焦点层 实现OnGlobalFocusChangeListener接口
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        mFocusLayout = new FocusLayout(this);
+        bindListener();//绑定焦点变化事件
+        addContentView(mFocusLayout,
+                new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT));//添加焦点层
+    }
+
+    private void bindListener() {
+        //获取根元素
+        View mContainerView = this.getWindow().getDecorView();//.findViewById(android.R.id.content);
+        //得到整个view树的viewTreeObserver
+        ViewTreeObserver viewTreeObserver = mContainerView.getViewTreeObserver();
+        //给观察者设置焦点变化监听
+        viewTreeObserver.addOnGlobalFocusChangeListener(mFocusLayout);
+    }
+}
